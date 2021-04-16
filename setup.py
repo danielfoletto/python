14f41c99e18967b421500b5c48dc07f5d7a95509
Daniel Foletto <danielfoletto@gmail.com>
2021-04-15 22:37:59 -0300

    setup git

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='danielfoletto',
+    author_email='danielfoletto@gmail.com',
+    description=''
+)
